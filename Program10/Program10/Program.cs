c834a25633487171d1558b3748212839a0397107
munshi-faysal <18203036@iubat.edu>
2022-03-22 01:40:32 +0600

    10 added

---
+++ Program10/Program10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Program10
+{
+    class ABCIceCream
+    {
+        public virtual void Icecream()
+        {
+            Console.WriteLine("Default Vanilla Icecream");
+        }
+    }
+    class XIceCream : ABCIceCream
+    {
+        public override void Icecream()
+        {
+            Console.WriteLine("Default Vanilla Icecream");
+        }
+    }
+    class YIceCream : ABCIceCream
+    {
+        public override void Icecream()
+        {
+                Console.WriteLine("Vanilla with Chocolate Icecream");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ABCIceCream abcIceCream = new ABCIceCream();
+            ABCIceCream xIceCreame = new XIceCream();
+            ABCIceCream yIceCreame = new YIceCream();
+
+            abcIceCream.Icecream();
+            xIceCreame.Icecream();
+            yIceCreame.Icecream();
+
+        }
+    }
+}
